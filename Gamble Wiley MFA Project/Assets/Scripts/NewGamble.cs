038ebfdd1d2f16fd7dc3d256b2ca6726d34a3e61
K9Gamer <71032019+K9Gamer@users.noreply.github.com>
2022-03-30 11:32:50 -0400

    import assets

---
+++ Gamble Wiley MFA Project/Assets/Scripts/NewGamble.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGamble : MonoBehaviour
+{
+    public Text CoinText;
+    public int Coins;
+    public GameObject gamble_screen1;//canvas?
+    public GameObject gamble_screen2;//canvas?
+    //sprites- images or gameobjects
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
