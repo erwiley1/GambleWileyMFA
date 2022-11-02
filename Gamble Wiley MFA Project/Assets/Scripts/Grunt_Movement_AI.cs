eb4d0a0cf369e3b1bf3a087b261fff2ad4017d32
CollinAnthony <CoAnthony@clarku.edu>
2022-11-02 12:44:37 -0400

    merge fixes

--- Gamble Wiley MFA Project/Assets/Scripts/Grunt_Movement_AI.cs
+++ Gamble Wiley MFA Project/Assets/Scripts/Grunt_Movement_AI.cs
@@ -9,9 +9,10 @@ public class Grunt_Movement_AI : MonoBehaviour
     public Animator animator; // grunt animations
     public float speed; // grunt movement speed
     public GameObject coinPrefab;
-    public int attack_damage; //currently unused, will be used to determine how much damage the attacks do
+    //public int attack_damage; currently unused, will be used to determine how much damage the attacks do
     public int attack_speed; //currently unused, will be used to determine how fast the enemies attack
 
+    private int spawn_location;
 
     Vector2 playerPos, enemyPos;
 
@@ -28,13 +29,32 @@ public class Grunt_Movement_AI : MonoBehaviour
 
     private void Awake() //applies the modifiers when this character is spawned
     {
-        attack_damage = 1; //for some reason i was experiencing a bug where these guys instantiated with 0 attack damage and attack speed and this fixed it
+        spawn_location = Random.Range(0, 5);
+        switch(spawn_location)
+        {
+            case 1:
+                transform.position = new Vector3(-32, 16);
+                break;
+            case 2:
+                transform.position = new Vector3(38, 16);
+                break;
+            case 3:
+                transform.position = new Vector3(22, -16);
+                break;
+            case 4:
+                transform.position = new Vector3(-26, -18);
+                break;
+            default:
+                transform.position = new Vector3(-32, 16);
+                break;
+        }
+        //attack_damage = 1; //for some reason i was experiencing a bug where these guys instantiated with 0 attack damage and attack speed and this fixed it
         attack_speed = 1;
-        Debug.Log(attack_damage);
+        //Debug.Log(attack_damage);
         modifiers = GameObject.FindGameObjectWithTag("Modifier Manager");
         speed *= modifiers.GetComponent<modifiers>().current_enemy_speed;
-        attack_damage *= modifiers.GetComponent<modifiers>().current_enemy_damage;
-        Debug.Log(attack_damage);
+        //attack_damage *= modifiers.GetComponent<modifiers>().current_enemy_damage;
+        //Debug.Log(attack_damage);
         attack_speed *= modifiers.GetComponent<modifiers>().current_enemy_attack_speed;
     }
 
@@ -114,10 +134,10 @@ public class Grunt_Movement_AI : MonoBehaviour
             }
         }
 
-        if (animator.GetBool("G_isDead"))
-        {
-            KillGrunt();
-        }
+        //if (animator.GetBool("G_isDead"))
+        //{
+        //    KillGrunt();
+        //}
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -136,7 +156,7 @@ public class Grunt_Movement_AI : MonoBehaviour
         if (animator.GetBool("G_isAttack")) { animator.SetBool("G_isAttack", false); }
     }
 
-    void KillGrunt()
+    void KillEnemy()
     {
         Instantiate(coinPrefab, transform.position, transform.rotation);
         Destroy(this.gameObject);
