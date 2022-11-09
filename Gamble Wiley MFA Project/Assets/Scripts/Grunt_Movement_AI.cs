e262d3da5c10d18f3a837312b5a3b6bb2bbd8ab3
CollinAnthony <CoAnthony@clarku.edu>
2022-11-08 22:34:55 -0500

    changing some code

--- Gamble Wiley MFA Project/Assets/Scripts/Grunt_Movement_AI.cs
+++ Gamble Wiley MFA Project/Assets/Scripts/Grunt_Movement_AI.cs
@@ -65,6 +65,8 @@ public class Grunt_Movement_AI : MonoBehaviour
         
         if (!animator.GetBool("G_isDead") && target != null) // if G_isDead is false and the player has a position somewhere on the board, then we can do things
         {
+            
+
             playerPos = new Vector2(target.localPosition.x, target.localPosition.y); // determines position of the player
             enemyPos = new Vector2(this.transform.localPosition.x, this.transform.localPosition.y); // determines position of the grunt
             
--- Gamble Wiley MFA Project/Assets/Test.cs
+++ Gamble Wiley MFA Project/Assets/Test.cs
@@ -26,7 +26,5 @@ public class Test : MonoBehaviour
 
     }
 
-
-
 }
 
